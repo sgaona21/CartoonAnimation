6a83c3ab49bc1c7c229ba819663de8b307437f16
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-03-29 21:04:48 -0700

    final touches

--- app/src/main/java/com/example/cartoonanimation/MainActivity.java
+++ app/src/main/java/com/example/cartoonanimation/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.cartoonanimation;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    AnimationDrawable ideaAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageView imgFrame = (ImageView)findViewById(R.id.cartoonImage);
+        imgFrame.setBackgroundResource(R.drawable.animation);
+        ideaAnimation = (AnimationDrawable)imgFrame.getBackground();
+
+        Button startButton = (Button)findViewById(R.id.startButton);
+        Button stopButton = (Button)findViewById(R.id.stopButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ideaAnimation.start();
+            }
         });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ideaAnimation.stop();
+                startActivity(new Intent(MainActivity.this, Tween.class));
+            }
+        });
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
